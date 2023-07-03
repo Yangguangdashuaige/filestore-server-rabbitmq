27495e4a967a2e24b18412dff6dc8c1f61e71169
Yangguangdashuaige <113884745+Yangguangdashuaige@users.noreply.github.com>
2023-07-03 19:42:42 +0800

    Add files via upload

---
+++ filestore-server-charter9/config/store.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	cmn "filestore-server/common"
+)
+
+const (
+	// TempLocalRootDir : 本地临时存储地址的路径
+	// TempLocalRootDir = "/data/fileserver/"
+	TempLocalRootDir = "/Users/samtake/Documents/GitHub/www/data/fileserver"
+	// CurrentStoreType : 设置当前文件的存储类型
+	CurrentStoreType = cmn.StoreLocal
+)
